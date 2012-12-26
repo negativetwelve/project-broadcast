9c3a2ec99e97c4f79d2b4fd3a91a6f4586219928
Mark Miyashita <negativetwelve@gmail.com>
2012-12-26 15:09:32 -0800

    base app with minified libraries

---
+++ readme.md
@@ -0,0 +1,21 @@
+Broadcaster.tv
+==============
+
+Real-time broadcasting and streaming.
+
+Built with Node.js, Backbone.js, MongoDB, and socket.io
+
+Running the App
+---------------
+To run the program:
+
+  npm install -d
+
+Runs on port 3000 unless otherwise specified.
+
+Dependencies
+------------
+
+* node.js
+* express
+* socket.io
---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var io = require('socket.io');
+var room = require('./routes/rooms');
+
+var app = express();
+
+app.configure(function () {
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+var server = http.createServer(app);
+io = io.listen(server);
+
+io.configure(function() {
+  io.set('authorization', function (handshakeData, callback) {
+    if (handshakeData.xdomain) {
+      callback('Cross-domain connections are not allowed');
+    } else {
+      callback(null, true);
+    }
+  });
+});
+
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/lounge', room.findAll);
+app.get('/:title', room.findByTitle);
+app.post('/lounge', room.addRoom);
+app.put('/:title', room.updateRoom);
+app.delete('/:title', room.deleteRoom);
+
+io.sockets.on('connection', function (socket) {
+  
+  socket.on('message', function (message) {
+    console.log('Got message: ' + message);
+  });
+  
+  socket.on('disconnect', function () {
+    console.log('Socket disconnected.');
+  });
+  
+  socket.on('updatePlaying', function (room, data) {
+    console.log('server received updatePlaying');
+    io.sockets.emit('updatePlaying', { 'videoInfo': data });
+  });
+  
+});
