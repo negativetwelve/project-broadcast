f6fb58389d0b7f12a938c4cfd8d3de0cd0213e40
Mark Miyashita <negativetwelve@gmail.com>
2012-12-28 02:38:34 -0800

    basic login system working

--- routes/room.js
+++ routes/room.js
@@ -11,11 +11,6 @@ var serverOptions = {
 var path = process.env.MONGOHQ_URL || 'mongohq://' + host + ':' + port + '/' + dbName;
 var db = mongoskin.db(path, serverOptions);
 
-db.collection('rooms').find().toArray(function (err, result) {
-    if (err) throw err;
-    console.log(result);
-});
-
 db.collection('rooms').find().toArray(function (err, rooms) {
   if (err || rooms.length === 0) {
     console.log('populating db');
--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.configure(function () {
   app.set('port', process.env.PORT || 3000);
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'the-best-secret-in-the-world' }));
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: app.root + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('view engine', 'ejs');
   app.set('views', __dirname + '/views');
@@ -35,15 +39,24 @@ server.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.get('/', function (req, res) {
-  res.redirect('/lounge');
-});
+//app.get('/', room.findAll);
+//app.post('/', room.addRoom);
+app.get('/', user.main);
+app.post('/', user.login);
+app.get('/home', user.home);
+app.post('/home', user.updateAccount);
+app.get('/signup', user.createAccount);
+app.post('/signup', user.signUp);
+app.post('/lost-password', user.passwordReset);
+app.get('/reset-password', user.getResetPasswordPage);
+app.post('/reset-password', user.submitResetPassword);
+app.get('/print', user.print);
+app.post('/delete', user.delete);
+app.get('/reset', user.reset);
 
-app.get('/lounge', room.findAll);
-app.get('/:title', room.findByTitle);
-app.post('/lounge', room.addRoom);
-app.put('/:title', room.updateRoom);
-app.delete('/:title', room.deleteRoom);
+app.get('/rooms/:title', room.findByTitle);
+app.put('/rooms/:title', room.updateRoom);
+app.delete('/rooms/:title', room.deleteRoom);
 
 io.sockets.on('connection', function (socket) {
   
